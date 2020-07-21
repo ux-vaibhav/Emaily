e7ad90b912d1711254faccab8e2fa46c81d5cf33
Vaibhav Panchal <ux.vaibhav@gmail.com>
2020-07-22 00:52:09 +0530

    Code Updated

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/user');
+require('./services/passport');
 
-app.get('/',(req,res)=>{
-    res.send({hi:'there'});
-})
+
+const app = express();
+mongoose.connect(keys.mongoURI);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);

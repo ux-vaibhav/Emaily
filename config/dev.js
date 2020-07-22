86752e8aa881fbaef7ff7cec256da4a66933d96a
Vaibhav Panchal <ux.vaibhav@gmail.com>
2020-07-22 23:26:55 +0530

    key update

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID:'909960029909-oc9oco5567t47ao4qe2qfgse2o2h4vf0.apps.googleusercontent.com',
+    googleClientSecret:'pwJtruXyUzcD7ymiiCgAkwu3',
+    mongoURI: 'mongodb+srv://admin:admin@emaily.mh3xe.mongodb.net/emaily?retryWrites=true&w=majority',
+    cookieKey:'dfgdfgfggfjsfdfgfhgfhghdsfsdsfdsf',
+}
+
+// 296412154672-0t4le5fc6jsg6va6h3b3oi5h2bjrmbb5.apps.googleusercontent.com
+//gfTB2F0GrVezdKOdb64clFWR
---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID: process.env.googleClientID,
+  googleClientSecret: process.env.googleClientSecret,
+  mongoURI: process.env.mongoURI,
+  cookieKey: process.env.cookieKey,
+}
+
+// 296412154672-0t4le5fc6jsg6va6h3b3oi5h2bjrmbb5.apps.googleusercontent.com
+//gfTB2F0GrVezdKOdb64clFWR

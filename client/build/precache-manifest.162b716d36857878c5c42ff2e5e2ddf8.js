747223be32061988a1b0c75c92d1d1c5b2e14c5e
Vaibhav Panchal <ux.vaibhav@gmail.com>
2020-07-27 17:23:49 +0530

    build added

---
+++ client/build/precache-manifest.162b716d36857878c5c42ff2e5e2ddf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df41068c2e1cb0626a2b9c80f6b505a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aba7d93e58cd0ac3aa78",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "aba7d93e58cd0ac3aa78",
+    "url": "/static/js/2.47b5285e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.47b5285e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1350e8cce6e90cb29a9a",
+    "url": "/static/js/main.d9adfa7e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);

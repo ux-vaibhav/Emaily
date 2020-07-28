7574061be06289149552314ce1ddc979b94f1fec
Vaibhav Panchal <ux.vaibhav@gmail.com>
2020-07-28 13:41:18 +0530

    build updated

---
+++ client/build/precache-manifest.eda21a806cc5435efc7b29d824b9d8cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55cdd880992f90fb4df21a868c2131d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1da6ea167ee2e619b2dd",
+    "url": "/static/js/2.6623ab1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6623ab1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e877dca835808333b63",
+    "url": "/static/js/main.1e690c60.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
